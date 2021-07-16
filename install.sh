66f9d3dd612db9abcb72ff535328098234a4051c
Yohei Shimmyo <qwpmb554@gmail.com>
2021-07-16 17:18:36 +0900

    change dirname into english

--- install.sh
+++ install.sh
@@ -75,6 +75,7 @@ LOCALE_CONF="LANG=ja_JP.UTF-8 LANGUAGE=ja_JP:en LC_CTYPE=ja_JP.UTF-8 LC_NUMERIC=
 
 chroot $MOUNT_POINT sh -c "$LOCALE_CONF apt update"
 chroot $MOUNT_POINT sh -c "$LOCALE_CONF apt install -y /home/pi/ome-packages/*.deb"
+chroot $MOUNT_POINT sh -c "apt install xdg-user-dirs-gtk ; LANG=C xdg-user-dirs-gtk-update --force
 
 # These are not necessary because raspberry pi does not actually launch and initial resize does not execute.
 # sed -i 's;$; init=/usr/lib/raspi-config/init_resize.sh;g' ${MOUNT_POINT}/boot/cmdline.txt
@@ -86,10 +87,6 @@ umount_sysfds
 
 umount $MOUNT_POINT/boot
 umount $MOUNT_POINT
-
-# DEBUG
-cp 2018-04-18-raspbian-stretch.img 2018-04-18-raspbian-stretch-before-shrink.img
-# END DEBUG
 
 # Truncate filesystem and partition
 e2fsck -f ${DEVICE_PATH}p2 && resize2fs -M ${DEVICE_PATH}p2
