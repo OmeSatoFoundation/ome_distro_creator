4e96ad31aef81cba8f1b5da3f39c2eb4bce52fc8
Yohei Shimmyo <qwpmb554@gmail.com>
2021-09-02 16:00:25 +0900

    exclude baseimage zip

--- clean.sh
+++ clean.sh
@@ -3,7 +3,7 @@ BASEIMAGE=2018-04-18-raspbian-stretch
 MOUNT_POINT=mount_point
 
 umount -R $MOUNT_POINT
-rm -rf $BASEIMAGE.zip $BASEIMAGE.img
+rm -rf $BASEIMAGE.img
 rm -rf ome2019 ome-doc ome-packages
 
 # TODO: parse `losetup` and detach loop devices related to $BASEIMAGE.img
