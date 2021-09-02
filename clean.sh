12969654dae2d5e0c810261dbb0f66ba4930dd7f
Yohei Shimmyo <qwpmb554@gmail.com>
2021-09-02 15:42:09 +0900

    clean.sh removes cloned repositories and the downloaded baseimage

--- clean.sh
+++ clean.sh
@@ -1,4 +1,9 @@
-sudo losetup -d /dev/loop12
-sudo umount /dev/loop12p2 mount_point
-rm -rf ./2018-04-18-raspbian-stretch.img
-rm -rf mount_point
+#!/bin/sh
+BASEIMAGE=2018-04-18-raspbian-stretch
+MOUNT_POINT=mount_point
+
+umount -R $MOUNT_POINT
+rm -rf $BASEIMAGE.zip $BASEIMAGE.img
+rm -rf ome2019 ome-doc ome-packages
+
+# TODO: paser `losetup` and detach loop devices related to $BASEIMAGE.img
